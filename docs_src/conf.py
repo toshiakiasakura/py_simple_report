47f77af208344977c99ace64213bb0b361897611
toshiakiasakura <aflu.blossompaws@gmail.com>
2022-03-13 19:09:23 +0900

    add edit on github

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -58,6 +58,14 @@ import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_context = {
+  'display_github': True,
+  'github_user': 'toshiakiasakura',
+  'github_repo': 'py_simple_report',
+  'github_version': 'main/docs_src/',
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
