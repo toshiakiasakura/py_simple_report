63da406f403797516d84813549b6f5bf1fa95a47
toshiakiasakura <aflu.blossompaws@gmail.com>
2022-03-23 23:54:45 +0900

    one more import function here.

--- py_simple_report/__init__.py
+++ py_simple_report/__init__.py
@@ -2,6 +2,7 @@
 from .main import (
     output_crosstab_cate_barplot,
     output_multi_binaries_with_strat,
+    question_data_containers_from_dataframe,
 )
 from .utils import delete_and_create_csv
 from .variables import QuestionDataContainer, VisVariables
